ab5d6cdd8c6e947a5fce21fb113eee15f26596a6
project11 <6640213108@nrru.ac.th>
2026-07-22 01:39:42 +0700

    update

--- php/quiz.php
+++ php/quiz.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-$conn = new mysqli('tokaido.proxy.rlwy.net', 'root', 'REDACTED_DB_PASSWORD', 'railway', 57745);
+$conn = new mysqli('tokaido.proxy.rlwy.net', 'root', 'REDACTED_DB_PASSWORD', 'railway', 23637);
 if ($conn->connect_error) {
     die('DB connection failed');
 }

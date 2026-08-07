9d9e44d4e30f8bd167169d0f03fc7c9a4556bbad
project11 <6640213108@nrru.ac.th>
2026-08-07 16:47:42 +0700

    update

--- php/debug_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode([
-    'session'    => $_SESSION,
-    'session_id' => session_id(),
-    'cookies'    => $_COOKIE,
-]);
-?>
